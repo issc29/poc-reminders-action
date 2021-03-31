01b21161c510c927b6c58f6046e5987cef7cdbd8
Isaac Cohen <issc29@gmail.com>
2021-03-31 13:47:06 -0400

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const dedent = require('dedent');
+const myToken = core.getInput('github-token');
+const octokit = github.getOctokit(myToken)
 const functionsLib = require('actions-api-functions');
 var functions = new functionsLib(octokit, core)
 const InProgressColumnID = core.getInput('InProgressColumnID');
